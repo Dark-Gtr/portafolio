87aa34778aa18fb1df4e0be95e2f8edacc03fb09
jose luis <64709809+Dark-Gtr@users.noreply.github.com>
2021-11-06 14:26:57 -0600

    agregé estilos para desktop al navbar

--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
 const colorTheme1 = ["#ff5722"]
 const colorTheme2 = ["#00bcd4"]
 
+// Aquí se implementa el tema de colores
 let html = document.querySelector("html");
 html.style.setProperty("--main-color", `${colorTheme1[0]}`);
-let buttonMenu = document.querySelector(".icon__menu-container");
 
 
+//funcion para activar los estilos que corresponden al menu
 const activateMenu = () => {
     document.querySelector(".main-header").classList.toggle('header-active');
     document.querySelector(".header-hidden").classList.toggle("header-visible");
     document.querySelector(".nav").classList.toggle("nav-active");
 }
 
+//funcion para dar scroll asta una seccion en especifico
 const scrollToDestino = (numero) => {
     document.querySelector(".main-container").children[numero].scrollIntoView({
         behavior: "smooth",
@@ -19,7 +21,7 @@ const scrollToDestino = (numero) => {
     })
 }
 
-buttonMenu.onclick = activateMenu
+document.querySelector(".icon__menu-container").onclick = activateMenu;
 
 const mainList = document.querySelectorAll(".list");
 
@@ -30,17 +32,100 @@ for (let i = 0; i < mainList.length; i++) {
             mainList[b++].className = "list";
         }
 
-        mainList[i].classList.toggle("list-active")
+        /* mainList[i].classList.toggle("list-active"); */
+
+        if (document.querySelector('html').clientWidth < 1024) {
+            activateMenu();
+        }
         
-        /* html.scrollT({
-            top: (totalHeigth / mainList.length) * i,
-            behavior: "smooth"
-        }) */
-        activateMenu()
         
-        scrollToDestino(i)
+        scrollToDestino(i);
         
         
     }
     
 }
+
+const height = document.querySelector('html').clientHeight;
+// Estas variables es para que las funciones se ejecuten una vez
+let switchHome = false;
+let switchAbout = false;
+let switchPortfolio = false;
+let switchContact = false;
+
+// Este es para que por defecto home inicie en activado
+mainList[0].classList.toggle('list-active');
+
+const activateListByScroll = () => {
+    if (window.scrollY < height && switchHome === false) {
+        switchHome = true;
+        switchAbout = false;
+        switchPortfolio = false;
+        switchContact = false;
+
+        if (mainList[1].classList.length > 1) {
+            mainList[1].classList.toggle('list-active');
+        }
+
+        if (mainList[0].classList.length != 2) {
+            mainList[0].classList.toggle('list-active');
+        }
+        
+
+    }
+    if(window.scrollY >= height && window.scrollY < (height * 2) && switchAbout === false){
+        switchHome = false;
+        switchAbout = true;
+        switchPortfolio = false;
+        switchContact = false;
+        
+        if (mainList[0].classList.length > 1) {
+            mainList[0].classList.toggle('list-active');
+        }
+        
+        if (mainList[1].classList.length != 2) {
+            mainList[1].classList.toggle('list-active');
+        }
+
+        if (mainList[2].classList.length > 1) {
+            mainList[2].classList.toggle('list-active');
+        }
+
+        
+    }
+    if (window.scrollY > (height * 2) && window.scrollY < (height * 3) && switchPortfolio === false) {
+        switchHome = false;
+        switchAbout = false;
+        switchPortfolio = true;
+        switchContact = false;
+
+        if (mainList[1].classList.length > 1) {
+            mainList[1].classList.toggle('list-active');
+        }
+        
+        if (mainList[2].classList.length != 2) {
+            mainList[2].classList.toggle('list-active');
+        }
+        
+        if (mainList[3].classList.length > 1) {
+            mainList[3].classList.toggle('list-active');
+        }
+    }
+    if (window.scrollY >= (height * 3) && switchContact === false) {
+        switchHome = false;
+        switchAbout = false;
+        switchPortfolio = false;
+        switchContact = true;
+
+        if (mainList[2].classList.length > 1) {
+            mainList[2].classList.toggle('list-active');
+        }
+        
+        if (mainList[3].classList.length != 2) {
+            mainList[3].classList.toggle('list-active');
+        }
+        
+    }
+}
+
+window.onscroll = activateListByScroll;
